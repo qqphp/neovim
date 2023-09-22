7e705aaa117d049e96f27a9479ba974c29e39541
kyle <1549684884@qq.com>
2023-09-22 01:39:09 +0000

    * Add commint.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,7 @@
 local options = {
     -- Example
-    autoindent = true,
+    autoindent = true, -- 自动缩进
+    scrolloff = 8, -- 光标距离顶部和底部的行数
     wrap = true, -- 自动换行
     relativenumber = false, -- 使用相对行号
     mouse = "", -- 禁用鼠标
