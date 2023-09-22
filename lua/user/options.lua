95aec221e89b55618c07cbcf197580ac7f1ef0da
kyle <1549684884@qq.com>
2023-09-22 02:09:05 +0000

    * Set scrolloff.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,7 @@
 local options = {
     -- Example
     autoindent = true, -- 自动缩进
-    scrolloff = 8, -- 光标距离顶部和底部的行数
+    scrolloff = 2, -- 光标距离顶部和底部的行数
     wrap = true, -- 自动换行
     relativenumber = false, -- 使用相对行号
     mouse = "", -- 禁用鼠标
